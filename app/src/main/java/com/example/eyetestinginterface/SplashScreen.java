3081106e87761529ce29c60b889df9b44cc94162
Pranav Prakasan <pranav.prakasan2019@gmail.com>
2021-03-31 00:11:34 +0530

    Initial commit

---
+++ app/src/main/java/com/example/eyetestinginterface/SplashScreen.java
@@ -0,0 +1,26 @@
+package com.example.eyetestinginterface;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Handler handler = new Handler();
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+            }
+        }, 4000);
+
+    }
+}
