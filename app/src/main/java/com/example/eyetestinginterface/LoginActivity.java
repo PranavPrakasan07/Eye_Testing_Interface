ac9a4bcb1f5b8b8422c6b40cc8ab10ebe3a74753
Pranav Prakasan <pranav.prakasan2019@gmail.com>
2021-03-31 00:43:41 +0530

    Passing data between login, signup implemented

--- app/src/main/java/com/example/eyetestinginterface/LoginActivity.java
+++ app/src/main/java/com/example/eyetestinginterface/LoginActivity.java
@@ -2,13 +2,62 @@ package com.example.eyetestinginterface;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText email, password;
+    TextView signup_link;
+
+    Button login_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+
+        signup_link = findViewById(R.id.signup_link);
+
+        login_button = findViewById(R.id.login_button);
+
+        Intent intent = getIntent();
+
+        try {
+            String email_text = intent.getStringExtra("email");
+            String password_text = intent.getStringExtra("password");
+
+            email.setText(email_text);
+            password.setText(password_text);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        signup_link.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SignUpActivity.class);
+
+                Bundle bundle = new Bundle();
+
+                String email_text = email.getText().toString();
+                String password_text = password.getText().toString();
+
+                bundle.putString("email", email_text);
+                bundle.putString("password", password_text);
+
+                intent.putExtras(bundle);
+
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/eyetestinginterface/SignUpActivity.java
+++ app/src/main/java/com/example/eyetestinginterface/SignUpActivity.java
@@ -2,17 +2,64 @@ package com.example.eyetestinginterface;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class SignUpActivity extends AppCompatActivity {
 
+    EditText email, password, contact;
+    TextView login_link;
+
+    Button signup_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        contact = findViewById(R.id.contact_number);
 
+        login_link = findViewById(R.id.login_link);
+
+        signup_button = findViewById(R.id.signup_button);
+
+        Intent intent = getIntent();
+
+        try {
+            String email_text = intent.getStringExtra("email");
+            String password_text = intent.getStringExtra("password");
+
+            email.setText(email_text);
+            password.setText(password_text);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        login_link.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+
+                Bundle bundle = new Bundle();
+
+                String email_text = email.getText().toString();
+                String password_text = password.getText().toString();
+
+                bundle.putString("email", email_text);
+                bundle.putString("password", password_text);
+
+                intent.putExtras(bundle);
+
+
+                startActivity(intent);
+            }
+        });
     }
 }
