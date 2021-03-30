bc61a9b04b2c39902854378308dc94058642811d
Pranav Prakasan <pranav.prakasan2019@gmail.com>
2021-03-31 00:30:47 +0530

    Signup UI created

---
+++ app/src/main/java/com/example/eyetestinginterface/SignUpActivity.java
@@ -0,0 +1,18 @@
+package com.example.eyetestinginterface;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class SignUpActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+
+    }
+}
