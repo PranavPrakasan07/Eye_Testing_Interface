d7285c99c1eb1952bfff41251a9c5103df0f1f8e
Pranav Prakasan <pranav.prakasan2019@gmail.com>
2021-04-11 16:15:37 +0530

    Navbar added

---
+++ app/src/main/java/com/example/eyetestinginterface/FindDoctorFragment.java
@@ -0,0 +1,64 @@
+package com.example.eyetestinginterface;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link FindDoctorFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class FindDoctorFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public FindDoctorFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment FindDoctorFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static FindDoctorFragment newInstance(String param1, String param2) {
+        FindDoctorFragment fragment = new FindDoctorFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_find_doctor, container, false);
+    }
+}
--- app/src/main/java/com/example/eyetestinginterface/Home.java
+++ app/src/main/java/com/example/eyetestinginterface/Home.java
@@ -1,14 +1,69 @@
 package com.example.eyetestinginterface;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.ismaeldivita.chipnavigation.ChipNavigationBar;
 
 public class Home extends AppCompatActivity {
+
+    ChipNavigationBar chipNavigationBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+
+        chipNavigationBar = findViewById(R.id.bottom_nav_bar);
+
+        chipNavigationBar.setItemSelected(R.id.nav_test, true);
+
+        Log.d("TAG : i", String.valueOf(chipNavigationBar.getSelectedItemId()));
+
+        chipNavigationBar.setOnItemSelectedListener(new ChipNavigationBar.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(int i) {
+
+            }
+        });
+
+        chipNavigationBar.setOnItemSelectedListener(new ChipNavigationBar.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(int i) {
+
+                Fragment selectedFragment = null;
+
+                if (chipNavigationBar.getSelectedItemId() == R.id.nav_find_doctor) {
+                    Toast.makeText(getApplicationContext(), "Add Selected", Toast.LENGTH_SHORT).show();
+                    Log.d("TAG : i", String.valueOf(i));
+
+                    selectedFragment = new FindDoctorFragment();
+                }
+
+                if (chipNavigationBar.getSelectedItemId() == R.id.nav_test) {
+                    Toast.makeText(getApplicationContext(), "Search Selected", Toast.LENGTH_SHORT).show();
+                    Log.d("TAG : i", String.valueOf(i));
+
+                    selectedFragment = new TestFragment();
+                }
+
+                if (chipNavigationBar.getSelectedItemId() == R.id.nav_profile) {
+                    Toast.makeText(getApplicationContext(), "Task Selected", Toast.LENGTH_SHORT).show();
+                    Log.d("TAG : i", String.valueOf(i));
+
+                    selectedFragment = new ProfileFragment();
+                }
+
+                assert selectedFragment != null;
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment, selectedFragment).commit();
+            }
+        });
+
     }
 }
